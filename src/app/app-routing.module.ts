5de787770345aacc486f29b535e4ad52bace0816
alberico <ocirebla@hotmail.com>
2022-03-21 22:24:44 -0300

    alterado rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,18 +2,18 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
+  // {
+  //   path: 'home',
+  //   loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  // },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'movie',
     pathMatch: 'full'
   },
   {
     path: 'movie',
-    loadChildren: () => import('./pages/movie/movie.module').then( m => m.MoviePageModule)
+    loadChildren: () => import('./pages/movies/movie.module').then( m => m.MoviePageModule)
   },
   {
     path: 'movie-details',
