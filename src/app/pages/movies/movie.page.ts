6d3f078a62b4caa69df81b824362c59e00238e49
alberico <ocirebla@hotmail.com>
2022-03-22 00:14:42 -0300

    adicionado search

--- src/app/pages/movies/movie.page.ts
+++ src/app/pages/movies/movie.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { MovieService, SearchType } from 'src/app/services/movie.service';
 
 @Component({
   selector: 'app-movie',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie.page.scss'],
 })
 export class MoviePage implements OnInit {
+  results: Observable<any>;
+  // eslint-disable-next-line @typescript-eslint/no-inferrable-types
+  searchTerm: string = '';
+  type: SearchType = SearchType.all;
 
-  constructor() { }
+  constructor(private movieService: MovieService) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  searchChanged(event: any) {
+    console.log('this.searchTerm,',this.searchTerm);
+    console.log('event',event);
+    // Call our service function which returns an Observable
+    this.results = this.movieService.searchData(this.searchTerm, this.type);
+    console.log('this.results', this.results);
   }
-
 }
--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export enum SearchType {
+  all = '',
+  movie = 'movie',
+  series = 'series',
+  episode = 'episode',
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MovieService {
+  url = 'http://www.omdbapi.com/';
+  apiKey = '444fe246'; // <-- Enter your own key here!
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  searchData(title: string, type: SearchType): Observable<any> {
+    const retorno = this.http
+      .get(
+        `${this.url}?s=${encodeURI(title)}&type=${type}&apikey=${this.apiKey}`
+      )
+      .pipe(
+        // eslint-disable-next-line @typescript-eslint/dot-notation
+        map((results) => results['Search'])
+      );
+
+    console.log('retorno', retorno);
+
+    return retorno;
+  }
+
+  getDetails(id) {
+    const retorno = this.http.get(
+      `${this.url}?i=${id}&plot=full&apikey=${this.apiKey}`
+    );
+    console.log('retorno', retorno);
+    return retorno;
+  }
 }
